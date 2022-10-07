e574a6d978683b334154aa391dbe257d81cf51ba
Adam Starbuck <adamstarbuck@pm.me>
2022-10-07 09:22:15 -0500

    Update printerlogic code

--- scripts/printerlogic code.sh
+++ scripts/printerlogic code.sh
@@ -1,21 +1,21 @@
-if [ -d "/opt/PrinterInstallerClient/" ] 
+if [ -d "/opt/PrinterInstallerClient/service_interface/PrinterInstallerClient.app" ] 
 then
     echo "PrinterLogic probably already installed!" 
-    HOMEURL="https://ourprintercloud.printercloud.com/"
+    HOMEURL="https://hiddenurl.printercloud.com/"
     CURRENTHOMEURL=$(cat /opt/PrinterInstallerClient/log/HOMEURL)
     if [ -n "$CURRENTHOMEURL" ] && [ "$HOMEURL" != "$CURRENTHOMEURL" ]; then
-        /opt/PrinterInstallerClient/bin/set_home_url.sh https ourprintercloud.printercloud.com/
+        /opt/PrinterInstallerClient/bin/set_home_url.sh https hiddenurl.printercloud.com/
     fi
     /opt/PrinterInstallerClient/bin/use_authorization_code.sh 1234abcd
     ls /opt/PrinterInstallerClient/tmp/requests/
     rm -r /opt/PrinterInstallerClient/tmp/requests/*
 else
-    echo "PrinterLogic probably not installed!"
-    curl -o /tmp/PrinterInstallerClientSetup.pkg "https://ourprintercloud.printercloud.com/client/setup/PrinterInstallerClientSetup.pkg" 
-    installer -allowUntrusted -pkg /tmp/PrinterInstallerClientSetup.pkg -target / 
-    rm /tmp/PrinterInstallerClientSetup.pkg
-    /opt/PrinterInstallerClient/bin/set_home_url.sh https ourprintercloud.printercloud.com/
-    /opt/PrinterInstallerClient/bin/use_authorization_code.sh 1234abcd
-    /opt/PrinterInstallerClient/bin/refresh.sh
+    echo "PrinterLogic probably not installed!" #this should not happen since we're using the Apps feature of Jamf School to install PrinterLogic
+    # curl -o /tmp/PrinterInstallerClientSetup.pkg "https://hiddenurl.printercloud.com/client/setup/PrinterInstallerClientSetup.pkg" 
+    # installer -allowUntrusted -pkg /tmp/PrinterInstallerClientSetup.pkg -target / 
+    # rm /tmp/PrinterInstallerClientSetup.pkg
+    # /opt/PrinterInstallerClient/bin/set_home_url.sh https hiddenurl.printercloud.com/
+    # /opt/PrinterInstallerClient/bin/use_authorization_code.sh 1234abcd
+    # /opt/PrinterInstallerClient/bin/refresh.sh
 fi
 
